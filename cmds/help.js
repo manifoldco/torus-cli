a44b79867748ee8e2d257b85753faf4532a6b9ee
Ian Livingstone <ianl@cs.dal.ca>
2015-11-07 23:44:10 -0400

    Properly handle exitCode and help text
    Exit codes are now assumed to be zero unless an error is encountered or
    the command returns a non-zero result.
    
    Help text is now stored inside the command file.

--- cmds/help.js
+++ cmds/help.js
@@ -2,5 +2,16 @@
 var log = require('../util/log').get('help');
 
 module.exports.command = function help (argv, cmds) {
-  log.print('Arigato amazing CLI tool');
+  
+  if (argv._.length === 0) {
+    return log.print('Arigato amazing CLI tool');
+  }
+
+  var target = cmds[argv._[0]];
+  if (target && target.helpText) {
+    return log.print(target.helpText);
+  }
+
+  log.error('Unknown Help Command: ', target);
+  return 1;
 };
--- cmds/whoami.js
+++ cmds/whoami.js
@@ -4,3 +4,7 @@ var log = require('../util/log').get('whoami');
 module.exports.command = function whoami (argv, cmds) {
   log.print('I have no fucking idea bro :)');
 };
+
+module.exports.helpText = `
+Hey this is some help text!
+`;
