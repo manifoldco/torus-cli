3d0572bdf41d10f6079763e599fb9682e75287dd
Ian Livingstone <ianl@cs.dal.ca>
2016-04-20 15:41:46 -0300

    Add env var for disabling daemon management
    Add an environment variable (`NO_DAEMON`) to skip starting and stopping
    the daemon through the middleware.
    
    Related #98

--- cli/lib/middleware/daemon.js
+++ cli/lib/middleware/daemon.js
@@ -1,9 +1,15 @@
 'use strict';
 
+var Promise = require('es6-promise').Promise;
 var daemon = require('../daemon');
 
 module.exports.preHook = function () {
   return function (ctx) {
+
+    if (process.env.NO_DAEMON) {
+      return Promise.resolve();
+    }
+
     return new Promise(function(resolve, reject) {
       if (!ctx.config) {
         return reject(new Error('Must have config property on Context'));
@@ -26,6 +32,10 @@ module.exports.preHook = function () {
 
 module.exports.postHook = function () {
   return function (ctx) {
+    if (process.env.NO_DAEMON) {
+      return Promise.resolve();
+    }
+
     if (!ctx.config || !ctx.daemon) {
       return Promise.reject(
         new Error('Must have config and daemon on Content'));
