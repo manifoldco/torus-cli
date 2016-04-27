2b5b6df1d052bec7eafba1a80d61f102c714f0e9
Jeff Andersen <jeffandersen@gmail.com>
2016-04-27 15:56:58 -0300

    Use a sinon.sandbox for tests

--- cli/tests/crypto/user.js
+++ cli/tests/crypto/user.js
@@ -3,7 +3,7 @@
 var sinon = require('sinon');
 var assert = require('assert');
 var Promise = require('es6-promise').Promise;
-var base64url = require('common/utils/base64url');
+var base64url = require('base64url');
 
 var kdf = require('../../lib/crypto/kdf');
 var user = require('../../lib/user/crypto');
@@ -13,28 +13,24 @@ var triplesec = require('../../lib/crypto/triplesec');
 var PLAINTEXT = 'password';
 var BUFFER = new Buffer('buffering');
 
-function restore(method) {
-  if (method.restore) {
-    method.restore();
-  }
-}
-
 describe('Crypto', function() {
+  before(function() {
+    this.sandbox = sinon.sandbox.create();
+  });
   describe('users', function() {
     afterEach(function() {
-      // Restore each method if necessary
-      [ kdf.generate,
-        utils.randomBytes,
-        triplesec.encrypt
-      ].forEach(restore);
+      this.sandbox.restore();
     });
 
-    describe('#encryptPassword', function() {
+    describe('#encryptPasswordObject', function() {
       it('generates 16byte salt for password', function() {
-        sinon.stub(kdf, 'generate').returns(Promise.resolve(BUFFER));
-        sinon.stub(utils, 'randomBytes').returns(Promise.resolve(BUFFER));
-        sinon.stub(triplesec, 'encrypt').returns(Promise.resolve(BUFFER));
-        return user.encryptPassword(PLAINTEXT).then(function() {
+        this.sandbox.stub(kdf, 'generate')
+          .returns(Promise.resolve(BUFFER));
+        this.sandbox.stub(utils, 'randomBytes')
+          .returns(Promise.resolve(BUFFER));
+        this.sandbox.stub(triplesec, 'encrypt')
+          .returns(Promise.resolve(BUFFER));
+        return user.encryptPasswordObject(PLAINTEXT).then(function() {
           sinon.assert.calledTwice(utils.randomBytes);
           var firstCall = utils.randomBytes.firstCall;
           assert.strictEqual(firstCall.args[0], 16);
@@ -43,10 +39,13 @@ describe('Crypto', function() {
 
       it('encrypts plaintext password with generated salt', function() {
         var value = new Buffer('this is the value');
-        sinon.stub(kdf, 'generate').returns(Promise.resolve(BUFFER));
-        sinon.stub(utils, 'randomBytes').returns(Promise.resolve(value));
-        sinon.stub(triplesec, 'encrypt').returns(Promise.resolve(BUFFER));
-        return user.encryptPassword(PLAINTEXT).then(function() {
+        this.sandbox.stub(kdf, 'generate')
+          .returns(Promise.resolve(BUFFER));
+        this.sandbox.stub(utils, 'randomBytes')
+          .returns(Promise.resolve(value));
+        this.sandbox.stub(triplesec, 'encrypt')
+          .returns(Promise.resolve(BUFFER));
+        return user.encryptPasswordObject(PLAINTEXT).then(function() {
           sinon.assert.calledOnce(kdf.generate);
           var firstCall = kdf.generate.firstCall;
           assert.strictEqual(firstCall.args[0], PLAINTEXT);
@@ -56,10 +55,13 @@ describe('Crypto', function() {
 
       it('generates 256byte key for master key', function() {
         var value = new Buffer('this is the value');
-        sinon.stub(kdf, 'generate').returns(Promise.resolve(BUFFER));
-        sinon.stub(utils, 'randomBytes').returns(Promise.resolve(value));
-        sinon.stub(triplesec, 'encrypt').returns(Promise.resolve(BUFFER));
-        return user.encryptPassword(PLAINTEXT).then(function() {
+        this.sandbox.stub(kdf, 'generate')
+          .returns(Promise.resolve(BUFFER));
+        this.sandbox.stub(utils, 'randomBytes')
+          .returns(Promise.resolve(value));
+        this.sandbox.stub(triplesec, 'encrypt')
+          .returns(Promise.resolve(BUFFER));
+        return user.encryptPasswordObject(PLAINTEXT).then(function() {
           sinon.assert.calledTwice(utils.randomBytes);
           var secondCall = utils.randomBytes.secondCall;
           assert.strictEqual(secondCall.args[0], 256);
@@ -68,10 +70,13 @@ describe('Crypto', function() {
 
       it('encrypts master key with password slice', function() {
         var value = new Buffer('this is the value');
-        sinon.stub(kdf, 'generate').returns(Promise.resolve(BUFFER));
-        sinon.stub(utils, 'randomBytes').returns(Promise.resolve(value));
-        sinon.stub(triplesec, 'encrypt').returns(Promise.resolve(BUFFER));
-        return user.encryptPassword(PLAINTEXT).then(function() {
+        this.sandbox.stub(kdf, 'generate')
+          .returns(Promise.resolve(BUFFER));
+        this.sandbox.stub(utils, 'randomBytes')
+          .returns(Promise.resolve(value));
+        this.sandbox.stub(triplesec, 'encrypt')
+          .returns(Promise.resolve(BUFFER));
+        return user.encryptPasswordObject(PLAINTEXT).then(function() {
           sinon.assert.calledOnce(triplesec.encrypt);
           var firstCall = triplesec.encrypt.firstCall;
           assert.deepEqual(firstCall.args[0], {
@@ -86,15 +91,15 @@ describe('Crypto', function() {
         var mkBytes = new Buffer('this is master key');
         var pwCipher = new Buffer('password cipher');
         var mkCipher = new Buffer('masterkey cipher');
-        sinon.stub(kdf, 'generate')
+        this.sandbox.stub(kdf, 'generate')
           .returns(Promise.resolve(pwCipher));
-        sinon.stub(utils, 'randomBytes')
+        this.sandbox.stub(utils, 'randomBytes')
           .onFirstCall().returns(Promise.resolve(pwBytes))
           .onSecondCall().returns(Promise.resolve(mkBytes));
-        sinon.stub(triplesec, 'encrypt')
+        this.sandbox.stub(triplesec, 'encrypt')
           .returns(Promise.resolve(mkCipher));
 
-        return user.encryptPassword(PLAINTEXT).then(function(obj) {
+        return user.encryptPasswordObject(PLAINTEXT).then(function(obj) {
           assert.deepEqual(obj, {
             password: {
               salt: base64url.encode(pwBytes),
