1924db4984a8c1f077b2b278a6d8ad3e61224296
Matt Creager <matt@arigato.sh>
2016-05-05 23:33:18 -0700

    add the logout command

---
+++ cli/lib/logout/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var Promise = require('es6-promise').Promise;
+var client = require('../api/client').create();
+
+/**
+ * Attempt logout
+ *
+ * @param {object} ctx - Prompt context
+ */
+module.exports = function(ctx) {
+  client.auth(ctx.token);
+
+  var resetClient = client.reset.bind(client);
+
+  return Promise.all([ ctx.daemon.logout(), client.post({ url: '/logout' })])
+    .then(resetClient)
+    .catch(function(err) {
+      resetClient();
+      throw err;
+    });
+};
