70e3dcbafa01593a992cd2b5402855d79c8a6725
Ian Livingstone <ianl@cs.dal.ca>
2016-07-12 11:45:59 -0300

    Reorganize container scripts and update README
    These scripts are used by both the daemon and cli, so let's make it
    clear at what level they live!

--- scripts/build-container.sh
+++ scripts/build-container.sh
@@ -5,10 +5,10 @@ set -eE
 set -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SRC=$DIR/../..
+SRC=$DIR/..
 NODEJS_VERSION=`node --version`
 
 pushd $SRC > /dev/null
   echo "Building docker container with node version: $NODEJS_VERSION"
-  docker build -t arigato/cli:latest --build-arg NODEJS_VERSION="$NODEJS_VERSION" .   
+  docker build -t arigato/cli:latest --build-arg NODEJS_VERSION="$NODEJS_VERSION" .
 popd > /dev/null
--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@ set -eE
 set -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SRC=$DIR/../..
+SRC=$DIR/..
 
 pushd $SRC > /dev/null
     # TODO: Derive the right values for GOOS and GOARCH based on `uname -a` and
