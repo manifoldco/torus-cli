f5eecaa5df37b6ba0d8314d017a8dda8127f23ca
Ian Livingstone <ianl@cs.dal.ca>
2016-07-26 14:24:40 -0300

    Do not start the daemon for prefs commands
    There is a race condition starting the deamon in development if the
    public_key_file has not been set as a preference. We dont require the
    daemon to run for prefs commands.
    
    Closes arigatomachine/cli#542

--- cli/lib/middleware/daemon.js
+++ cli/lib/middleware/daemon.js
@@ -6,6 +6,13 @@ var daemon = require('../daemon');
 module.exports.preHook = function () {
   return function (ctx) {
     return new Promise(function (resolve, reject) {
+      // The daemon won't start in some circumstances without a public_key_file
+      // preference set. We can't set a preference if we require the daemon to
+      // be running!
+      if (ctx.group === 'prefs') {
+        return resolve();
+      }
+
       if (!ctx.config) {
         return reject(new Error('Must have config property on Context'));
       }
