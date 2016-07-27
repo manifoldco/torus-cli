6ae314173054d4b8cd7e35b7281e2e0b9c228322
Matt Creager <matt@arigato.sh>
2016-07-27 18:21:53 -0300

    Add deny command

---
+++ cli/lib/cmds/deny.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var Promise = require('es6-promise').Promise;
+
+var Command = require('../cli/command');
+
+var deny = require('../access/deny');
+var auth = require('../middleware/auth');
+
+var command = new Command(
+  'deny',
+  'Explicitly deny a team permissions access to a secret[\'s]',
+  function (ctx) {
+    return new Promise(function (resolve, reject) {
+      deny.execute(ctx).then(function (payload) {
+        deny.output.success(null, payload);
+
+        resolve();
+      }).catch(function (err) {
+        err.type = err.type || 'unknown';
+        deny.output.failure();
+        reject(err);
+      });
+    });
+  }
+);
+
+command.hook('pre', auth());
+
+module.exports = command;
