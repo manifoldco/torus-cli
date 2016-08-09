18fb7e7afe4878d909e9127bab90cee2c550f92d
Ian Livingstone <ianl@cs.dal.ca>
2016-08-08 23:34:13 -0300

    WIP: Actually do something after the org invite

--- daemon/routes/org_invites.go
+++ daemon/routes/org_invites.go
@@ -4,6 +4,7 @@ package routes
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -12,6 +13,7 @@ import (
 	"github.com/arigatomachine/cli/daemon/crypto"
 	"github.com/arigatomachine/cli/daemon/db"
 	"github.com/arigatomachine/cli/daemon/identity"
+	"github.com/arigatomachine/cli/daemon/primitive"
 	"github.com/arigatomachine/cli/daemon/registry"
 	"github.com/arigatomachine/cli/daemon/session"
 )
@@ -32,6 +34,36 @@ func orgInvitesApproveRoute(client *registry.Client, s session.Session,
 			log.Printf("Could not approve org invite: %s", err)
 			encodeResponseErr(w, err)
 			return
+		}
+
+		// Get the organization claim tree; but only the keys for the invitee
+		inviteBody := invite.Body.(*primitive.OrgInvite)
+		claimTrees, err := client.ClaimTree.List(inviteBody.OrgID, inviteBody.InviteeID)
+		if err != nil {
+			log.Printf("could not retrieve claim tree for invite approval: %s", err)
+			encodeResponseErr(w, err)
+			return
+		}
+
+		if len(claimTrees) != 1 {
+			log.Printf("incorrect number of claim trees returned: %d", len(claimTrees))
+			encodeResponseErr(w, fmt.Errorf(
+				"No claim tree found for org: %s", inviteBody.OrgID))
+			return
+		}
+
+		// Get all the keyrings and memberships for the current user. This way we
+		// can decrypt the MEK for each and then create a new KeyringMember for
+		// our wonderful and new org member!
+		keyringSections, err := client.Keyring.List(inviteBody.OrgID, s.ID())
+		if err != nil {
+			log.Printf("could not retrieve keyring sections for user: %s", err)
+			encodeResponseErr(w, err)
+			return
+		}
+
+		if len(keyringSections) != 0 {
+			log.Printf("we need to do some real wokr here")
 		}
 
 		enc := json.NewEncoder(w)
