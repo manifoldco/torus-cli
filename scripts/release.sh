1c14bebe9e790d1bb773a8cec22890d6b6f33760
Ian Livingstone <ianl@cs.dal.ca>
2016-08-16 17:50:36 -0300

    Upload releases to s3
    All releases are uploaded to `releases.arigato.sh` bucket if the release
    runner agrees. If a release is not uploaded to s3 then it's not uploaded
    to npm.
    
    A user must be logged into aws cli to upload a release.
    
    Closes arigatomachine/cli#743

--- scripts/release.sh
+++ scripts/release.sh
@@ -28,6 +28,7 @@ CA_BUNDLE="$SRC/daemon/ca_bundle.pem"
 RELEASE_STAMP=`date -u +"%Y-%m-%dT%H-%M-%SZ"`
 BUILD_DIRECTORY=$HOME/build
 RELEASE_DIRECTORY="$BUILD_DIRECTORY/$RELEASE_STAMP"
+RELEASE_BUCKET="releases.arigato.sh"
 
 if [ ! -f "$KEY_FILE" ]; then
     echo "Cannot find offline public key file: $KEY_FILE"
@@ -37,6 +38,11 @@ fi
 if [ ! -d "$BUILD_DIRECTORY" ]; then
     echo "Build directory $BUILD_DIRECTORY does not exist; creating."
     mkdir -p $BUILD_DIRECTORY
+fi
+
+if ! aws iam get-user > /dev/null; then
+    echo "You must be logged into the aws cli to publish a release"
+    exit 1
 fi
 
 # Fetch the latest greatest and then figure out the pointer of the tag
@@ -86,15 +92,30 @@ echo "Creating Distributable ($RELEASE_STAMP.tar.gz) in $RELEASE_DIRECTORY"
 echo ""
 tar czf "$RELEASE_STAMP.tar.gz" cli/
 
-read -p "Do you wish to publish to npm? [yn] " publish
-case $publish in
-    [Yy]* )
-        npm publish $RELEASE_STAMP.tar.gz
+read -p "Do you wish to publish the release to s3? [yn]" s3_publish
+case $s3_publish in
+    [Yy]*)
+        echo "Uploading $RELEASE_STAMP to https://s3.amazonaws.com/$RELEASE_BUCKET/$TAR_FILENAME"
+        aws s3api put-object --bucket $RELEASE_BUCKET --key "$TAR_FILENAME" \
+            --body "$RELEASE_STAMP.tar.gz"
         ;;
-    * )
-        echo "Not publishing"
+    *)
+        echo "Not publishing to s3; exiting.. cannot publish to other sources"
+        exit 1
         ;;
 esac
+
+if [ "$ENVIRONMENT" == "production" ]; then
+    read -p "Do you wish to publish to npm? [yn] " npm_publish
+    case $npm_publish in
+        [Yy]* )
+            npm publish $RELEASE_STAMP.tar.gz
+            ;;
+        * )
+            echo "Not publishing to npm"
+            ;;
+    esac
+fi
 
 echo ""
 echo "Done; package $RELEASE_DIRECTORY/$RELEASE_STAMP.tar.gz"
