d4ff3330df6cee95325ddfea3c1537a8d39d46f4
Ian Livingstone <ianl@cs.dal.ca>
2016-08-15 21:18:37 -0300

    Support logging in using environment variables
    In some situations it's desirable to log into `ag` using environment
    variables (inside a docker container for example).
    
    Related arigatomachine/cli#762

--- cli/lib/middleware/auth.js
+++ cli/lib/middleware/auth.js
@@ -2,6 +2,8 @@
 
 var Promise = require('es6-promise').Promise;
 
+var login = require('../login/index');
+
 module.exports = function () {
   return function (ctx) {
     return new Promise(function (resolve, reject) {
@@ -10,7 +12,28 @@ module.exports = function () {
       ctx.api.session.get().then(function () {
         ctx.loggedIn = true;
         return resolve();
-      }).catch(function () {
+      }).catch(function (err) {
+        if (err.type !== 'unauthorized') {
+          return reject(err);
+        }
+
+        // if the email and password are set in the environment; then we can
+        // try and login with them!
+        if (process.env.AG_EMAIL && process.env.AG_PASSWORD) {
+          var inputs = {
+            email: process.env.AG_EMAIL,
+            passphrase: process.env.AG_PASSWORD
+          };
+
+          console.log('Attempting to login with email:', inputs.email);
+          return login.subcommand(ctx, inputs).then(function () {
+            console.log('Logged in successfully!\n');
+
+            ctx.loggedIn = true;
+            return resolve();
+          }).catch(reject);
+        }
+
         console.log('You must be logged-in to execute \'' +
                     name + ' ' + slug + '\'');
         console.log();
