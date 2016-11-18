e551fb45d97146a3980c614dd0666855c57f49f9
Ian Livingstone <ianl@cs.dal.ca>
2016-11-18 15:10:10 -0400

    Introduce torus template command
    The `torus template` command allows a user to inject secrets stored in
    torus as they derive a configuration file from a template.
    
    The current templating engine is the [raymond
    handlebars](https://github.com/aymerick/raymond) engine for Go
    which supports v3 of the handlebars spec.
    
    Also includes bumps to our dependencies, as I had to include the raymond
    library. The interface for `readline.NewCancelableStdin` has changed in
    newer versions which I've upgraded us to.
    
    Closes manifoldco/torus-cli#51

---
+++ cmd/template.go
@@ -0,0 +1,142 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/aymerick/raymond"
+	"github.com/urfave/cli"
+
+	"github.com/manifoldco/torus-cli/apitypes"
+	"github.com/manifoldco/torus-cli/errs"
+)
+
+func init() {
+	template := cli.Command{
+		Name:      "template",
+		Usage:     "Generate a config file from a template using secrets stored inside torus",
+		ArgsUsage: "<template-file> [output-file]",
+		Category:  "SECRETS",
+		Flags: []cli.Flag{
+			stdOrgFlag,
+			stdProjectFlag,
+			stdEnvFlag,
+			serviceFlag("Use this service.", "default", true),
+			userFlag("Use this user.", false),
+			machineFlag("Use this machine.", false),
+			stdInstanceFlag,
+		},
+		Action: chain(
+			ensureDaemon, ensureSession, loadDirPrefs, loadPrefDefaults,
+			setUserEnv, checkRequiredFlags, templateCmd,
+		),
+	}
+
+	Cmds = append(Cmds, template)
+}
+
+func templateCmd(ctx *cli.Context) error {
+
+	outputPath := ""
+	args := ctx.Args()
+
+	if len(args) == 0 {
+		return errs.NewUsageExitError("A template-file must be provided", ctx)
+	} else if len(args) > 2 {
+		return errs.NewUsageExitError("Too many arguments provided", ctx)
+	}
+
+	templatePath, err := filepath.Abs(args[0])
+	if err != nil {
+		return errs.NewErrorExitError("Invalid template file path", err)
+	}
+	if len(args) == 2 {
+		outputPath, err = filepath.Abs(args[1])
+		if err != nil {
+			return errs.NewErrorExitError("Invalid output file path", err)
+		}
+	}
+
+	template, err := loadTemplateFile(templatePath)
+	if err != nil {
+		return err
+	}
+
+	secrets, _, err := getSecrets(ctx)
+	if err != nil {
+		return err
+	}
+
+	output, err := deriveTemplateOutput(template, secrets)
+	if err != nil {
+		return err
+	}
+
+	if outputPath != "" {
+		return writeOutputFile(outputPath, output)
+	}
+
+	fmt.Printf("%s", output)
+	return nil
+}
+
+func loadTemplateFile(templatePath string) (string, error) {
+	buf, err := ioutil.ReadFile(templatePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return "", fmt.Errorf("Could not open template file: %s", templatePath)
+		}
+
+		return "", err
+	}
+
+	return string(buf), nil
+}
+
+// We expose the variables in two ways to the template:
+//
+//  1) Through a _config variable which has a Name and Value property for iterating
+//  2) Directly through the {{ slug }} and {{ Upper(slug) }} for straight referencing
+func deriveTemplateOutput(template string, secrets []apitypes.CredentialEnvelope) (string, error) {
+	variables := make(map[string]interface{})
+	configItems := make([]map[string]string, len(secrets))
+
+	for i, secret := range secrets {
+		value := (*secret.Body).GetValue().String()
+		name := (*secret.Body).GetName()
+		configItems[i] = map[string]string{
+			"name":  name,
+			"value": value,
+		}
+
+		variables[name] = value
+		variables[strings.ToUpper(name)] = value
+	}
+
+	// Export the config inside the template under `_config` without colliding
+	// with the variable namespace
+	variables["_config"] = configItems
+
+	result, err := raymond.Render(template, variables)
+	if err != nil {
+		return "", errs.NewErrorExitError("Could not generate template", err)
+	}
+
+	return result, nil
+}
+
+func writeOutputFile(outputPath, output string) error {
+	err := ioutil.WriteFile(outputPath, []byte(output), 0600)
+	if err != nil {
+		if os.IsExist(err) {
+			return fmt.Errorf("File already exists at output path: %s", outputPath)
+		}
+
+		return err
+	}
+
+	return nil
+}
