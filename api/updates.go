4c48e0955606178ba577a8e51f5b53da3611d183
Federico Ruggi <reifcode@gmail.com>
2017-01-17 18:53:29 +0100

    added godoc lines

--- api/updates.go
+++ api/updates.go
@@ -6,10 +6,14 @@ import (
 	"github.com/manifoldco/torus-cli/apitypes"
 )
 
+// UpdatesClient checks if there are updates available coming from
+// the daemon update checker component.
 type UpdatesClient struct {
 	client *Client
 }
 
+// Check returns the latest updates check result, useful for detecting whether
+// a newer version of Torus is available for download.
 func (c *UpdatesClient) Check(ctx context.Context) (*apitypes.Updates, error) {
 	req, _, err := c.client.NewRequest("GET", "/updates", nil, nil, false)
 	if err != nil {
