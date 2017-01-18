ee6518dbc5e726ae2141b4abd9d88cc46c7baaeb
James Bowes <jbowes@repl.ca>
2017-01-18 16:19:55 -0400

    Populate rich worklog details
    For each worklog item type, attach a Details struct containing type
    specifid information (such as teams pending for an invite, etc).
    
    Use some of this data for generating the item subject and summary string
    in the cli, rather than the daemon.
    
    This data will be used for presenting extra data in the worklog, and
    formatting it in a most joyful manner.

--- apitypes/worklog.go
+++ apitypes/worklog.go
@@ -2,11 +2,14 @@ package apitypes
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/dchest/blake2b"
 
 	"github.com/manifoldco/torus-cli/base32"
 	"github.com/manifoldco/torus-cli/identity"
+	"github.com/manifoldco/torus-cli/pathexp"
+	"github.com/manifoldco/torus-cli/primitive"
 )
 
 // WorklogType is the enumerated byte type of WorklogItems
@@ -73,11 +76,118 @@ func (id WorklogID) Type() WorklogType {
 // WorklogItem is an item that the daemon has identified as needing to be done
 // to ensure system correctness, or security in the face of stale secrets.
 type WorklogItem struct {
-	ID      *WorklogID `json:"id"`
-	Subject string     `json:"subject"`
-	Summary string     `json:"summary"`
+	ID *WorklogID `json:"id"`
 
-	SubjectID *identity.ID `json:"subject_id"` // Optional.
+	Details WorklogDetails `json:"details"`
+}
+
+// Subject returns the human readable subject of this WorklogItem.
+func (w *WorklogItem) Subject() string {
+	return w.Details.Subject()
+}
+
+// Summary returns the human readable summary of this WorklogItem.
+func (w *WorklogItem) Summary() string {
+	return w.Details.Summary()
+}
+
+// WorklogDetails is the common interface exposed by worklog item types.
+type WorklogDetails interface {
+	Subject() string
+	Summary() string
+}
+
+// InviteApproveWorklogDetails holds WorklogItem details for the
+// InviteApproveWorklogType.
+type InviteApproveWorklogDetails struct {
+	InviteID *identity.ID `json:"invite_id"`
+	Email    string       `json:"email"`
+	Username string       `json:"username"`
+	Name     string       `json:"name"`
+	Org      string       `json:"org"`
+	Teams    []string     `json:"teams"`
+}
+
+// Subject returns the human readable subject of this WorklogItem.
+func (i *InviteApproveWorklogDetails) Subject() string {
+	return i.Email
+}
+
+// Summary returns the human readable summary of this WorklogItem.
+func (i *InviteApproveWorklogDetails) Summary() string {
+	summary := fmt.Sprintf("The invite for %s to org %s is ready for approval.",
+		i.Email, i.Org)
+	return summary
+}
+
+// KeyringMembersWorklogDetails holds WorklogItem details for the
+// KeyringMembersWorklogType.
+type KeyringMembersWorklogDetails struct {
+	EntityID *identity.ID      `json:"entity_id"`
+	Name     string            `json:"name"`
+	Type     string            `json:"type"`
+	OwnerIDs []identity.ID     `json:"owner_ids"`
+	Keyrings []pathexp.PathExp `json:"keyrings"`
+}
+
+// Subject returns the human readable subject of this WorklogItem.
+func (k *KeyringMembersWorklogDetails) Subject() string {
+	return k.Name
+}
+
+// Summary returns the human readable summary of this WorklogItem.
+func (k *KeyringMembersWorklogDetails) Summary() string {
+	return fmt.Sprintf("This %s is missing access to one or more secrets.", k.Type)
+}
+
+// MissingKeypairsWorklogDetails holds WorklogItem details for the
+// MissingKeypairsWorklogType..
+type MissingKeypairsWorklogDetails struct {
+	Org               string `json:"org"`
+	EncryptionMissing bool   `json:"encryption_missing"`
+	SigningMissing    bool   `json:"signing_missing"`
+}
+
+// Subject returns the human readable subject of this WorklogItem.
+func (m *MissingKeypairsWorklogDetails) Subject() string {
+	return m.Org
+}
+
+// Summary returns the human readable summary of this WorklogItem.
+func (m *MissingKeypairsWorklogDetails) Summary() string {
+	msg := "Signing and encryption keypairs missing for org %s."
+	if !m.EncryptionMissing {
+		msg = "Signing keypair missing for org %s."
+	} else if !m.SigningMissing {
+		msg = "Encryption keypair missing for org %s."
+	}
+
+	return fmt.Sprintf(msg, m.Org)
+}
+
+// SecretRotateWorklogDetails holds WorklogItem details for the
+// SecretRotateWorklogType.
+type SecretRotateWorklogDetails struct {
+	PathExp *pathexp.PathExp            `json:"pathexp"`
+	Name    string                      `json:"name"`
+	Reasons []SecretRotateWorklogReason `json:"reasons"`
+}
+
+// SecretRotateWorklogReason holds the username and claim revocation type
+// for a secret rotation reason.
+type SecretRotateWorklogReason struct {
+	Username string                                `json:"username"`
+	Type     primitive.KeyringMemberRevocationType `json:"type"`
+}
+
+// Subject returns the human readable subject of this WorklogItem.
+func (s *SecretRotateWorklogDetails) Subject() string {
+	return s.PathExp.String() + "/" + s.Name
+}
+
+// Summary returns the human readable summary of this WorklogItem.
+func (s *SecretRotateWorklogDetails) Summary() string {
+	return "A user's access was revoked. This secret's value should be changed."
 }
 
 // Type returns this item's type
@@ -110,7 +220,7 @@ func (w *WorklogItem) CreateID(worklogType WorklogType) {
 	}
 
 	h.Write([]byte{byte(worklogType)})
-	h.Write([]byte(w.Subject))
+	h.Write([]byte(w.Details.Subject()))
 
 	id := WorklogID{byte(worklogType)}
 	copy(id[1:], h.Sum(nil))
