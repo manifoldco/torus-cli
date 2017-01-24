5f93958ce7c97e6043e093df4b13f82122921897
James Bowes <jbowes@repl.ca>
2017-01-24 19:15:55 -0400

    Teach the ui package to spawn indented children
    ui is not struct based, with a default UI instance availble from the
    package functions.
    
    `Child` can be called to get a new ui instance, which will indent
    anything it writes by the given amount.
    
    Switch to ansiwrap for text wrapping, which has indenting built in.

--- ui/ui.go
+++ ui/ui.go
@@ -2,57 +2,107 @@ package ui
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/chzyer/readline"
-	"github.com/kr/text"
+	"github.com/manifoldco/ansiwrap"
 
 	"github.com/manifoldco/torus-cli/prefs"
+	"github.com/manifoldco/torus-cli/promptui"
 )
 
-// enableProgress is whether progress events should be displayed
-var enableProgress = false
+const (
+	defaultCols = 80
+	rightPad    = 2
+)
 
-// enableHints is whether hints should be displayed
-var enableHints = false
+var bold = promptui.Styler(promptui.FGBold)
+var defUI *UI
 
-// Init prepares the ui preferences
+// Init initializes a default global UI, accessible via the package functions.
 func Init(preferences *prefs.Preferences) {
-	enableProgress = preferences.Core.EnableProgress
-	enableHints = preferences.Core.EnableHints
+	defUI = &UI{
+		Indent: 0,
+		Cols:   screenWidth(),
+
+		EnableProgress: preferences.Core.EnableProgress,
+		EnableHints:    preferences.Core.EnableHints,
+	}
 }
+
+// UI exposes methods for creating a terminal ui
+type UI struct {
+	Indent int
+
+	// Cols holds the column width for text wrapping. For the default UI and
+	// its children, It is either the width of the  terminal, or defaultCols,
+	// minus rightPad.
+	Cols int
+
+	// EnableProgress is whether progress events should be displayed
+	EnableProgress bool
+
+	// EnableHints is whether hints should be displayed
+	EnableHints bool
+}
+
+// Progress calls Progress on the default UI
+func Progress(str string) { defUI.Progress(str) }
 
 // Progress handles the ui output for progress events, when enabled
-func Progress(str string) {
-	if !enableProgress {
+func (u *UI) Progress(str string) {
+	if !u.EnableProgress {
 		return
 	}
-	fmt.Println(str)
+	u.Line(str)
 }
 
+// Line calls Line on the default UI
+func Line(format string, a ...interface{}) { defUI.Line(format, a...) }
+
+// Line writes a formatted string followed by a newline to stdout. Output is
+// word wrapped, and terminated by a newline.
+func (u *UI) Line(format string, a ...interface{}) {
+	o := fmt.Sprintf(format, a...)
+	fmt.Println(ansiwrap.WrapIndent(o, u.Cols, u.Indent, u.Indent))
+}
+
+// Hint calls hint on the default UI
+func Hint(str string, noPadding bool) { defUI.Hint(str, noPadding) }
+
 // Hint handles the ui output for hint/onboarding messages, when enabled
-func Hint(str string, noPadding bool) {
-	if !enableHints {
+func (u *UI) Hint(str string, noPadding bool) {
+	if !u.EnableHints {
 		return
 	}
 	if !noPadding {
-		fmt.Println("")
+		fmt.Println()
 	}
-	printWrapLabeled("Protip:", str)
+
+	label := bold("Protip: ")
+	rc := ansiwrap.RuneCount(label)
+	fmt.Println(ansiwrap.WrapIndent(label+str, u.Cols, u.Indent, u.Indent+rc))
 }
 
-func printWrapLabeled(label, message string) {
-	cols := readline.GetScreenWidth() - 2
-	fmt.Printf("%s  ", label)
-	longest := len(label)
-	wrapped := text.Wrap(message, cols-(2+longest))
-	fmt.Println(indentOthers(wrapped, 2+longest))
+// Child calls Child on the default UI
+func Child(indent int) *UI { return defUI.Child(indent) }
+
+// Child returns a new UI, with settings from the receiver UI, and Indent
+// increased by the provided value.
+func (u *UI) Child(indent int) *UI {
+	return &UI{
+		Indent: u.Indent + indent,
+		Cols:   u.Cols,
+
+		EnableProgress: u.EnableProgress,
+		EnableHints:    u.EnableHints,
+	}
 }
 
-func indentOthers(str string, indent int) string {
-	nl := strings.IndexRune(str, '\n')
-	if nl == -1 {
-		nl = len(str)
+func screenWidth() int {
+	w := readline.GetScreenWidth()
+	if w <= 0 {
+		w = defaultCols
 	}
-	return str[:nl] + text.Indent(str[nl:], fmt.Sprintf("%*s", indent, ""))
+
+	return w - rightPad
 }
