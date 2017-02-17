f37a9bc91df03aef9f96fcd41002fe6eb510c745
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2017-02-17 17:11:30 +0100

    Windows: set homedrive accordingly.

--- config/root_windows.go
+++ config/root_windows.go
@@ -10,7 +10,7 @@ const requiredPermissions = 0777
 func torusRootPath() string {
 	torusRoot := os.Getenv("TORUS_ROOT")
 	if len(torusRoot) == 0 {
-		torusRoot = path.Join("C:\\", os.Getenv("HomePath"), ".torus")
+		torusRoot = path.Join(os.Getenv("HOMEDRIVE"), os.Getenv("HomePath"), ".torus")
 	}
 
 	return torusRoot
