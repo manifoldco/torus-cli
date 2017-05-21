684902d3be9db2cb15c03c09c050f6166e6791b5
Dan Enman <enmand@gmail.com>
2017-05-21 20:43:53 -0300

    Rename gatekeeper/routes/machines.go -> bootstrap.go
    Clearer indication of what the route is for (e.g. not a route for any
    machine creation)

--- gatekeeper/routes/boostrap.go
+++ gatekeeper/routes/boostrap.go
@@ -6,7 +6,8 @@ import (
 	"github.com/manifoldco/torus-cli/api"
 )
 
-func machineCreateRoute(api *api.Client) http.HandlerFunc {
+// awsMachineBoot
+func awsBootstrapRoute(api *api.Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("ok"))
 	}
