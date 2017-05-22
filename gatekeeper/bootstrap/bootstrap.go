386a3ba0e5fca429b400fc61e5897e248dc152a7
Dan Enman <enmand@gmail.com>
2017-05-21 21:33:16 -0300

    Rename auth package to bootstrap

--- gatekeeper/bootstrap/bootstrap.go
+++ gatekeeper/bootstrap/bootstrap.go
@@ -1,10 +1,11 @@
 // Package auth provides authentication actions for the bootstrap process
-package auth
+package bootstrap
 
 import (
 	"fmt"
 
-	"github.com/manifoldco/torus-cli/gatekeeper/auth/aws"
+	"github.com/manifoldco/torus-cli/gatekeeper/apitypes"
+	"github.com/manifoldco/torus-cli/gatekeeper/bootstrap/aws"
 )
 
 const (
@@ -17,13 +18,13 @@ type Type string
 // Provider is an interface that provides bootstrapping and verification of a role
 type Provider interface {
 	// Bootstrap provides the main bootstrapping functions for the auth Provider
-	Bootstrap(url, org, role string) error
+	Bootstrap(url, org, role string) (*apitypes.BootstrapResponse, error)
 
-	// Verify does backend verification on the auth Provider
+	// Verify does backend verification on the bootstrap Provider
 	Verify() error
 }
 
-// New returns a new Provider based on the given auth.Type
+// New returns a new Provider based on the given bootstrap.Type
 func New(t Type) (Provider, error) {
 	switch string(t) {
 	case AWSPublic:
