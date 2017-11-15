29ace29f2950019d639588044920e73ddb000e70
morochena <marcus.orochena@gmail.com>
2017-11-15 13:36:26 -0500

    initial security attributes for named pipe attempt

--- api/transport_windows.go
+++ api/transport_windows.go
@@ -4,14 +4,14 @@ import (
 	"net"
 	"net/http"
 
+	"github.com/Microsoft/go-winio"
 	"github.com/manifoldco/torus-cli/config"
-	"github.com/natefinch/npipe"
 )
 
 func newTransport(cfg *config.Config) *http.Transport {
 	return &http.Transport{
 		Dial: func(network, address string) (net.Conn, error) {
-			return npipe.DialTimeout(cfg.TransportAddress, dialTimeout)
+			return winio.DialPipe(cfg.TransportAddress, nil)
 		},
 	}
 }
