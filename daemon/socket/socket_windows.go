a7172d6181d4cf91a9da11bfdca8ceb7cc3af519
morochena <marcus.orochena@gmail.com>
2017-12-07 21:36:25 -0500

    moved to dep, updated error logging

--- daemon/socket/socket_windows.go
+++ daemon/socket/socket_windows.go
@@ -4,6 +4,7 @@ package socket
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os/user"
 
@@ -11,14 +12,14 @@ import (
 )
 
 func makeSocket(transportAddress string, groupShared bool) (net.Listener, error) {
-	// Gets current user's SID 
+	// Gets current user's SID
 	usr, err := user.Current()
 	if err != nil {
-		fmt.Printf("Error getting user SID: %s\n", transportAddress)
+		log.Printf("Error getting user SID: %s\n", err)
 		return nil, err
 	}
 
-	// Configures pipe security descriptor to allow full control for system, administrators and current user. 
+	// Configures pipe security descriptor to allow full control for system, administrators and current user.
 	// Security Descriptor String Format
 	// https://msdn.microsoft.com/en-us/library/windows/desktop/aa379570(v=vs.85).aspx
 	c := winio.PipeConfig{
